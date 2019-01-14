0ba6298bda78ac12442b284d36f710e11925e0f2
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-14 21:18:40 +0700

    Enums and Color

--- the_runesmith/src/main/java/runesmith/RunesmithMod.java
+++ the_runesmith/src/main/java/runesmith/RunesmithMod.java
@@ -1,16 +1,34 @@
 package runesmith;
 
+import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+import basemod.BaseMod;
+import runesmith.patches.AbstractCardEnum;
 
 
 @SpireInitializer
 public class RunesmithMod {
+	
+	private static Color BEIGE = new Color(245f/255f, 245f/255f, 220f/255f, 1f);
 
-		public RunesmithMod() {
-			
-		}
+	public RunesmithMod() {
 		
-		public static void initialize() {
-			new RunesmithMod();
-		}
+	}
+		
+	public static void initialize() {
+		BaseMod.addColor(
+				AbstractCardEnum.RUNESMITH_BEIGE,
+				BEIGE,
+				"", //attackBg
+				"", //skillBg
+				"", //powerBg
+				"", //energyOrb
+				"", //attackBgPortrait
+				"", //skillBgPortrait
+				"", //powerBgPortrait
+				"", //energyOrbPortrait
+				""  ); //cardEnergyOrb
+		new RunesmithMod();
+	}
 }
---
+++ the_runesmith/src/main/java/runesmith/patches/LibraryTypeEnum.java
@@ -0,0 +1,10 @@
+package runesmith.patches;
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+
+public class LibraryTypeEnum {
+
+	@SpireEnum
+	public static CardLibrary.LibraryType RUNESMITH;
+	
+}
