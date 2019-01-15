93c9793e7d0feeb3253f250757d13b5726ba6fc5
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-16 03:41:50 +0700

    fixed power png paths

--- the_runesmith/src/main/java/runesmith/powers/AquaPower.java
+++ the_runesmith/src/main/java/runesmith/powers/AquaPower.java
@@ -1,8 +1,10 @@
 package runesmith.powers;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
@@ -21,8 +23,8 @@ public class AquaPower extends AbstractPower {
 		this.priority = 2;
 		this.type = AbstractPower.PowerType.BUFF;
 		updateDescription();
-		this.img = new Texture("images/powers/xxx.png"); //<--------------Some img things
-
+		this.region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("images/powers/Aqua.png"), 0, 0, 84, 84);
+	    this.region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("images/powers/AquaSmall.png"), 0, 0, 32, 32);
 	}
 	
 	public void stackPower(int stackAmount) {
