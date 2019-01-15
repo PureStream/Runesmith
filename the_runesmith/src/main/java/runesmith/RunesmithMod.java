d2895613dbe79fcaa5172346608572b4fd1a341b
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-15 16:14:11 +0700

    Add json file for all descriptions and etc.

--- the_runesmith/src/main/java/runesmith/RunesmithMod.java
+++ the_runesmith/src/main/java/runesmith/RunesmithMod.java
@@ -1,8 +1,19 @@
 package runesmith;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.Keyword;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.BaseMod;
 import basemod.interfaces.EditCharactersSubscriber;
@@ -10,19 +21,54 @@ import runesmith.character.player.RunesmithCharacter;
 import runesmith.patches.AbstractCardEnum;
 import runesmith.patches.PlayerClassEnum;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditCharactersSubscriber;
+import basemod.interfaces.EditKeywordsSubscriber;
+import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import basemod.interfaces.OnCardUseSubscriber;
+import basemod.interfaces.OnPowersModifiedSubscriber;
+import basemod.interfaces.PostBattleSubscriber;
+import basemod.interfaces.PostDrawSubscriber;
+import basemod.interfaces.PostDungeonInitializeSubscriber;
+import basemod.interfaces.PostExhaustSubscriber;
+import basemod.interfaces.PostInitializeSubscriber;
+
 
 @SpireInitializer
-public class RunesmithMod implements EditCharactersSubscriber{
+public class RunesmithMod implements PostExhaustSubscriber,
+	PostBattleSubscriber,
+	PostDungeonInitializeSubscriber,
+	EditCharactersSubscriber,
+	PostInitializeSubscriber,
+	EditRelicsSubscriber,
+	EditCardsSubscriber,
+	EditStringsSubscriber,
+	OnCardUseSubscriber,
+	EditKeywordsSubscriber,
+	OnPowersModifiedSubscriber,
+	PostDrawSubscriber {
+	
+	public static final Logger logger = LogManager.getLogger(RunesmithMod.class.getName());
 	
 	private static final String RUNESMITH_BUTTON = null;
 	private static final String RUNESMITH_PORTRAIT = null;
 	private static Color BEIGE = new Color(245f/255f, 245f/255f, 220f/255f, 1f);
+	
+	private static final String CARD_STRING = "localization/RuneSMod_Cards.json";
+	private static final String RELIC_STRING = "localization/RuneSMod_Relics";
+	private static final String POWER_STRING = "localization/RuneSMod_Powers";
+	private static final String POTION_STRING = "localization/RuneSMod_Potions";
+	private static final String KEYWORD_STRING = "localization/RuneSMod_Keywords";
 
 	public RunesmithMod() {
-		
-	}
-		
-	public static void initialize() {
+		BaseMod.subscribe(this);
+		logger.info("creating the color : RUNESMITH_BEIGE");
 		BaseMod.addColor(
 				AbstractCardEnum.RUNESMITH_BEIGE,
 				BEIGE,
@@ -35,14 +81,190 @@ public class RunesmithMod implements EditCharactersSubscriber{
 				"", //powerBgPortrait
 				"", //energyOrbPortrait
 				""  ); //cardEnergyOrb
+	}
+		
+	public static void initialize() {
+		
 		new RunesmithMod();
 	}
 	
 	@Override
 	public void receiveEditCharacters() {
+		logger.info("begin editing characters");
 		BaseMod.addCharacter(new RunesmithCharacter(CardCrawlGame.playerName),
 				RUNESMITH_BUTTON,
 				RUNESMITH_PORTRAIT,
 				PlayerClassEnum.RUNESMITH_CLASS);
+		logger.info("done editing characters");
+	}
+	
+	@Override
+	public void receiveEditStrings() {
+	    logger.info("start editing strings");
+	
+		String relicStrings, cardStrings, powerStrings, potionStrings, relic, card, power, potion;
+		
+		logger.info("lang == eng");
+		card = CARD_STRING;
+		relic = RELIC_STRING;
+		power = POWER_STRING;
+		potion = POTION_STRING;
+		
+		/*if (Settings.language == Settings.GameLanguage.ZHS) {
+		  logger.info("lang == zhs");
+		  card = CARD_STRING_ZH;
+		  relic = RELIC_STRING_ZH;
+		  power = POWER_STRING_ZH;
+		  potion = POTION_STRING_ZH;
+		} else if (Settings.language == Settings.GameLanguage.JPN) {
+		  logger.info("lang == jpn");
+		  card = CARD_STRING_JP;
+		  relic = RELIC_STRING_JP;
+		  power = POWER_STRING_JP;
+		  potion = POTION_STRING_JP;
+		} else if (Settings.language == Settings.GameLanguage.ZHT) {
+		  logger.info("lang == zht");
+		  card = CARD_STRING_ZHT;
+		  relic = RELIC_STRING_ZHT;
+		  power = POWER_STRING_ZHT;
+		  potion = POTION_STRING_ZHT;
+		} else if (Settings.language == Settings.GameLanguage.KOR) {
+		  logger.info("lang == kor");
+		  card = CARD_STRING_KR;
+		  relic = RELIC_STRING_KR;
+		  power = POWER_STRING_KR;
+		  potion = POTION_STRING_KR;
+		} else if(Settings.language == Settings.GameLanguage.FRA) {
+		  logger.info("lang == fra");
+		  card = CARD_STRING_FR;
+		  relic = RELIC_STRING_FR;
+		  power = POWER_STRING_FR;
+		  potion = POTION_STRING_FR;
+		} else {
+		  logger.info("lang == eng");
+		  card = CARD_STRING;
+		  relic = RELIC_STRING;
+		  power = POWER_STRING;
+		  potion = POTION_STRING;
+		}*/
+		
+		relicStrings = Gdx.files.internal(relic).readString(
+		    String.valueOf(StandardCharsets.UTF_8)
+		);
+		BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
+		cardStrings = Gdx.files.internal(card).readString(
+		    String.valueOf(StandardCharsets.UTF_8)
+		);
+		BaseMod.loadCustomStrings(CardStrings.class, cardStrings);
+		powerStrings = Gdx.files.internal(power).readString(
+		    String.valueOf(StandardCharsets.UTF_8)
+		);
+		BaseMod.loadCustomStrings(PowerStrings.class, powerStrings);
+		potionStrings = Gdx.files.internal(potion).readString(
+		    String.valueOf(StandardCharsets.UTF_8)
+		);
+		BaseMod.loadCustomStrings(PotionStrings.class, potionStrings);
+		
+		logger.info("done editing strings");
+	}
+
+	@Override
+	public void receiveEditKeywords() {
+		logger.info("Setting up custom keywords");
+
+	    String keywordsPath;
+	    /*switch (Settings.language) {
+	      case ZHT:
+	        keywordsPath = KEYWORD_STRING_ZHT;
+	        break;
+	      case ZHS:
+	        keywordsPath = KEYWORD_STRING_ZHS;
+	        break;
+	      case KOR:
+	        keywordsPath = KEYWORD_STRING_KR;
+	        break;
+	      case JPN:
+	        keywordsPath = KEYWORD_STRING_JP;
+	        break;
+	      case FRA:
+	      keywordsPath = KEYWORD_STRING_FR;
+	      break;
+	      default:
+	        keywordsPath = KEYWORD_STRING;
+	        break;
+	    }*/
+	    keywordsPath = KEYWORD_STRING;
+
+	    Gson gson = new Gson();
+	    Keywords keywords;
+	    keywords = gson.fromJson(loadJson(keywordsPath), Keywords.class);
+	    for (Keyword key : keywords.keywords) {
+	      logger.info("Loading keyword : " + key.NAMES[0]);
+	      BaseMod.addKeyword(key.NAMES, key.DESCRIPTION);
+	    }
+	    logger.info("Keywords setting finished.");
+	}
+	
+	private static String loadJson(String jsonPath) {
+		return Gdx.files.internal(jsonPath).readString(String.valueOf(StandardCharsets.UTF_8));
+	}
+	
+	class Keywords {
+	    Keyword[] keywords;
+	}
+
+	@Override
+	public void receivePostDraw(AbstractCard arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivePowersModified() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveCardUsed(AbstractCard arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveEditCards() {
+		logger.info("begin editting cards");
+	    logger.info("add cards for the Runesmith");
+		
+	}
+
+	@Override
+	public void receiveEditRelics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivePostInitialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivePostDungeonInitialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivePostBattle(AbstractRoom arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivePostExhaust(AbstractCard arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
