05cb11aa8f747f9e4b5f489a2e46a26780058513
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-16 16:34:16 +0700

    fix some dumb mistakes?

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftFirestone.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftFirestone.java
@@ -4,6 +4,7 @@ import basemod.helpers.BaseModCardTags;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -11,7 +12,6 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import basemod.abstracts.CustomCard;
-import runesmith.actions.CraftAction;
 import runesmith.actions.RuneChannelAction;
 import runesmith.orbs.FirestoneRune;
 import runesmith.patches.AbstractCardEnum;
@@ -48,19 +48,19 @@ public class CraftFirestone extends AbstractRunicCard {
 	}
 	
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		/*if(p.hasPower("IgnisPower")) {
+		if(p.hasPower("IgnisPower")) {
 			if(p.getPower("IgnisPower").amount>=2) {
 				AbstractDungeon.actionManager.addToBottom(
 						new RuneChannelAction(
 								new FirestoneRune(this.potency)));
 				p.getPower("IgnisPower").reducePower(2);
+				AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(p, p, "IgnisPower"));
+				
 			}else addPower();
-		}else addPower();*/
-		AbstractDungeon.actionManager.addToBottom(
-				new CraftAction(p,"Runesmith:CraftFirestone",2,0,0,2));
+		}else addPower();
 	}
 	
-	/*public void addPower() {
+	public void addPower() {
 		AbstractDungeon.actionManager.addToTop(
 		          new ApplyPowerAction(
 		              AbstractDungeon.player,
@@ -69,7 +69,7 @@ public class CraftFirestone extends AbstractRunicCard {
 		              IGNIS_AMT
 		          )
 		      );
-	}*/
+	}
 	
 	public AbstractCard makeCopy() {
 		return new CraftFirestone();
