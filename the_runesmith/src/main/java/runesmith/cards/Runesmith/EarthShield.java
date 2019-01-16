8f329aface5484f7d6c409efaff258b29b9ae677
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-16 21:01:44 +0700

    Merge, add keywords and Complete Craft Incendium

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/EarthShield.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/EarthShield.java
@@ -12,6 +12,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import basemod.abstracts.CustomCard;
 import runesmith.patches.AbstractCardEnum;
 import runesmith.powers.IgnisPower;
+import runesmith.powers.TerraPower;
 
 public class EarthShield extends CustomCard {
 	public static final String ID = "Runesmith:EarthShield";
@@ -44,7 +45,7 @@ public class EarthShield extends CustomCard {
 			new GainBlockAction(p, p, this.block)
 		);
 		AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, 
-				new IgnisPower(p, TERRA_AMT),TERRA_AMT));
+				new TerraPower(p, TERRA_AMT),TERRA_AMT));
 	}
 	
 	public AbstractCard makeCopy() {
