d9f66780498155eccc53fa815a2524bba4472dcb
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-16 21:32:42 +0700

    combine earthshield

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/EarthShield.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/EarthShield.java
@@ -45,7 +45,7 @@ public class EarthShield extends CustomCard {
 			new GainBlockAction(p, p, this.block)
 		);
 		AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, 
-				new IgnisPower(p, TERRA_AMT),TERRA_AMT));
+				new TerraPower(p, TERRA_AMT),TERRA_AMT));
 	}
 	
 	public AbstractCard makeCopy() {
