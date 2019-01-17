d5e23aa00937844b768e61b060c32200e914e4c1
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-17 17:28:54 +0700

    fixing stasis

--- the_runesmith/src/main/java/runesmith/patches/StasisExhaustPrevention.java
+++ the_runesmith/src/main/java/runesmith/patches/StasisExhaustPrevention.java
@@ -13,14 +13,17 @@ import com.megacrit.cardcrawl.vfx.cardManip.CardDarkFlashVfx;
 import runesmith.RunesmithMod;
 
 public class StasisExhaustPrevention {
-	public static final Logger logger = LogManager.getLogger(EnhancedCardValueModified.class.getName());
+	public static final Logger logger = LogManager.getLogger(StasisExhaustPrevention.class.getName());
 	
 	@SpirePatch(clz = CardGroup.class, method = "moveToExhaustPile")
 	public static class preventExhaust {
-		public static void Prefix(CardGroup self, AbstractCard c) {
+		public static void Postfix(CardGroup self, AbstractCard c) {
 			if(CardStasisStatus.isStatis.get(c)) {
+				logger.info("Attempting exhaust prevention");
 				AbstractDungeon.effectList.add(new CardDarkFlashVfx(c, RunesmithMod.BEIGE));
 				CardStasisStatus.isStatis.set(c, false);
+				AbstractDungeon.player.exhaustPile.removeTopCard();
+				AbstractDungeon.player.discardPile.addToTop(c);
 				return;
 			}
 		}
