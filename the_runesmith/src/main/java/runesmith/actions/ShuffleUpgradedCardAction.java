583ded09a13a0fd726eb850e65df13eac17bacb7
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-18 04:59:16 +0700

    Add Rearm but Rearm is still now working

--- the_runesmith/src/main/java/runesmith/actions/ShuffleUpgradedCardAction.java
+++ the_runesmith/src/main/java/runesmith/actions/ShuffleUpgradedCardAction.java
@@ -1,9 +1,9 @@
 package runesmith.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ShuffleAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class ShuffleUpgradedCardAction extends AbstractGameAction {
@@ -13,18 +13,23 @@ public class ShuffleUpgradedCardAction extends AbstractGameAction {
 		this.p = AbstractDungeon.player;
 		setValues(this.p, AbstractDungeon.player, this.amount);
 		this.actionType = ActionType.CARD_MANIPULATION;
+		this.duration = Settings.ACTION_DUR_FAST;
 	}
 	
 	public void update() {
-		if (this.p.discardPile.size() > 0) {
-			for (AbstractCard card : this.p.discardPile.group) {
-				if (card.upgraded) {
-					p.drawPile.addToRandomSpot(card);
-					p.discardPile.removeCard(card);
+		if (this.duration == Settings.ACTION_DUR_FAST) {
+			if (this.p.discardPile.size() > 0) {
+				for (AbstractCard card : this.p.discardPile.group) {
+					if (card != null) {
+						if (card.upgraded && p.discardPile.contains(card)) {
+							p.drawPile.addToRandomSpot(card);;
+							p.discardPile.removeCard(card);
+						}
+					}
 				}
 			}
 		}
-		AbstractDungeon.actionManager.addToBottom(new ShuffleAction(p.drawPile, false));
+		//AbstractDungeon.actionManager.addToBottom(new ShuffleAction(p.drawPile, false));
 		tickDuration();
 		this.isDone = true;
 	}
