efb406c38fcf0d8bee8d54c58bf0256abdb0f2ac
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-17 16:15:22 +0700

    Add HAMMER and CRAFT Tag

---
+++ the_runesmith/src/main/java/runesmith/patches/CardTagEnum.java
@@ -0,0 +1,10 @@
+package runesmith.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class CardTagEnum {
+	@SpireEnum
+	public static AbstractCard.CardTags HAMMER;
+	public static AbstractCard.CardTags CRAFT;
+}
