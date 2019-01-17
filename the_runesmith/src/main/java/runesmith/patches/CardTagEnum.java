bf08fec2b6002613bbdaedd43dc9ae4b1afd363f
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-17 16:16:38 +0700

    Fix a bit on Tag

--- the_runesmith/src/main/java/runesmith/patches/CardTagEnum.java
+++ the_runesmith/src/main/java/runesmith/patches/CardTagEnum.java
@@ -4,7 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class CardTagEnum {
-	@SpireEnum
-	public static AbstractCard.CardTags HAMMER;
-	public static AbstractCard.CardTags CRAFT;
+	
+	@SpireEnum public static AbstractCard.CardTags HAMMER;
+	@SpireEnum public static AbstractCard.CardTags CRAFT;
 }
