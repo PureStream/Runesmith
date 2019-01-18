8ddd78567a2b49d067728bb579cf4519a8ba0cc7
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-19 03:40:03 +0700

    Add LastStand

---
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/LastStand.java
@@ -0,0 +1,81 @@
+package runesmith.cards.Runesmith;
+
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import basemod.abstracts.CustomCard;
+import runesmith.actions.DowngradeEntireDeckAction;
+import runesmith.patches.AbstractCardEnum;
+
+public class LastStand extends CustomCard {
+	public static final String ID = "Runesmith:LastStand";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String IMG_PATH = "images/cards/LastStand.png"; //<-------------- need some img
+	private static final int COST = 3;
+	private static final int MULTIPLIER_AMT = 7;
+	private static final int UPG_MULTIPLIER_AMT = 4;
+
+	public LastStand() {
+		super(
+			ID,
+			NAME,
+			IMG_PATH,
+			COST,
+			DESCRIPTION,
+			CardType.ATTACK,
+			AbstractCardEnum.RUNESMITH_BEIGE,
+			CardRarity.RARE,
+			CardTarget.ALL_ENEMY
+		);
+		
+		this.baseDamage = 0;
+		this.isMultiDamage = true;
+		this.baseMagicNumber = this.magicNumber = MULTIPLIER_AMT;
+		
+	}
+
+	public void use(AbstractPlayer p, AbstractMonster m) {
+		int upgradedCount = 0;
+		for(AbstractCard c : p.discardPile.group) {
+			if(c.upgraded) {
+				upgradedCount++;
+			}
+		}
+		for(AbstractCard c : p.drawPile.group) {
+			if(c.upgraded) {
+				upgradedCount++;
+			}
+		}
+		for(AbstractCard c : p.hand.group) {
+			if(c.upgraded) {
+				upgradedCount++;
+			}
+		}
+		AbstractDungeon.actionManager.addToBottom(
+				new DowngradeEntireDeckAction(p));
+		this.damage = upgradedCount*this.magicNumber;
+		AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.utility.SFXAction("ATTACK_HEAVY"));
+		AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new com.megacrit.cardcrawl.vfx.combat.MindblastEffect(p.dialogX, p.dialogY, p.flipHorizontal), 0.1F));
+		AbstractDungeon.actionManager.addToBottom(
+			new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect.NONE)
+		);
+	}
+
+	public AbstractCard makeCopy() {
+		return new LastStand();
+	}
+
+	public void upgrade() {
+		if (!this.upgraded) {
+		  upgradeName();
+		  upgradeMagicNumber(UPG_MULTIPLIER_AMT);
+		}
+	}
+}
