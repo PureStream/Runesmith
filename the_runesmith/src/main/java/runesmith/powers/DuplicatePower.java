8963ba9d1e12748837a73fcac03280bf7d070abd
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-19 22:41:20 +0700

    Remove unused import

--- the_runesmith/src/main/java/runesmith/powers/DuplicatePower.java
+++ the_runesmith/src/main/java/runesmith/powers/DuplicatePower.java
@@ -2,19 +2,12 @@ package runesmith.powers;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-
-import static runesmith.patches.CardTagEnum.CRAFT;
 
 public class DuplicatePower extends AbstractPower {
 
