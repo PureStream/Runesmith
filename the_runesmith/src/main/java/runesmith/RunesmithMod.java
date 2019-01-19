87bfca6c0c9202714d60b838f43c823c92eec1f1
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-19 18:21:39 +0700

    Add Construct Bifrost to the game

--- the_runesmith/src/main/java/runesmith/RunesmithMod.java
+++ the_runesmith/src/main/java/runesmith/RunesmithMod.java
@@ -326,6 +326,7 @@ public class RunesmithMod implements PostExhaustSubscriber,
 		cardsToAdd.add(new GrandSlam());
 		cardsToAdd.add(new ForcesOfNature());
 		cardsToAdd.add(new LastStand());
+		cardsToAdd.add(new ConstructBifrost());
 	}
 
 	@Override
