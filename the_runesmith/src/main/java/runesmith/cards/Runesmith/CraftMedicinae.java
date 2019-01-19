84f23843c946c286b2d680e4ebcc3dc2e61a617c
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-19 23:45:35 +0700

    Buff Medicinae

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftMedicinae.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftMedicinae.java
@@ -21,7 +21,7 @@ public class CraftMedicinae extends AbstractRunicCard {
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
-	private static final int COST = 2;
+	private static final int COST = 1;
 	private static final int POTENCY = 3;
 	private static final int UPG_POTENCY = 1;
 	private static final int TERRA_AMT = 5;
