3a23be71bc9ecf9231a1245c696820c1c11daf58
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-21 00:36:01 +0700

    changed + rendering on some runes

--- the_runesmith/src/main/java/runesmith/orbs/ReservoRune.java
+++ the_runesmith/src/main/java/runesmith/orbs/ReservoRune.java
@@ -76,7 +76,7 @@ public class ReservoRune extends RuneOrb {
 			//render upgrade +
 			FontHelper.renderFontCentered(sb, FontHelper.cardEnergyFont_L, 
 					"+", this.cX + NUM_X_OFFSET, this.cY + this.bobEffect.y / 2.0F + NUM_Y_OFFSET ,
-					Color.GREEN.cpy(), this.fontScale*2);
+					Color.GREEN.cpy(), this.fontScale);
 		}
 	}
 }
