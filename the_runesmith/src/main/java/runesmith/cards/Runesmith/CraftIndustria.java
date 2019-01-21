6909ec60921ca02ebab69938a0a68b8eff5ab0dd
Blizzarre <Blizzarre@users.noreply.github.com>
2019-01-22 00:55:34 +0700

    fixed industria

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftIndustria.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/CraftIndustria.java
@@ -20,7 +20,7 @@ public class CraftIndustria extends AbstractRunicCard {
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+//	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 	public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
 	private static final int COST = 1;
 	private static final int AQUA_AMT = 4;
