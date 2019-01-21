e995ded0dc8e10be737f8ce5a2b6e63c99d2d5fd
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-21 13:52:10 +0700

    Remove unused var, import

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/GenerateForcefield.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/GenerateForcefield.java
@@ -1,12 +1,7 @@
 package runesmith.cards.Runesmith;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardRarity;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardTarget;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -29,7 +24,6 @@ public class GenerateForcefield extends CustomCard {
 	public static final String DESCRIPTION_UPG = cardStrings.UPGRADE_DESCRIPTION;
 	public static final String IMG_PATH = "images/cards/GenerateForcefield.png"; //need some img
 	private static final int COST = -1;
-	private static final int SKILL_AMT = 1;
 
 	public GenerateForcefield() {
 		super(
