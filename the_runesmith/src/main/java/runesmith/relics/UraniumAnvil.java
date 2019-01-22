cb1a871a459fbf92fa40b950155a1d30dd066864
Siwakorn <InfiniteSoRa@outlook.com>
2019-01-22 15:00:24 +0700

    Add Uranium Anvil code

---
+++ the_runesmith/src/main/java/runesmith/relics/UraniumAnvil.java
@@ -0,0 +1,56 @@
+package runesmith.relics;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import basemod.abstracts.CustomRelic;
+import runesmith.actions.DowngradeRandomCardInDeckAction;
+
+public class UraniumAnvil extends CustomRelic {
+	
+	public static final String ID = "Runesmith:UraniumAnvil";
+	private static final String IMG = "images/relics/UraniumAnvil.png"; //<--------- Need some img
+	private static final int DOWN_AMT = 1;
+	
+	public UraniumAnvil() {
+		super(ID, ImageMaster.loadImage(IMG), RelicTier.BOSS, LandingSound.CLINK); 
+	}
+	
+	public String getUpdatedDescription() {
+		if (com.megacrit.cardcrawl.dungeons.AbstractDungeon.player != null) {
+			return setDescription(com.megacrit.cardcrawl.dungeons.AbstractDungeon.player.chosenClass);
+		}
+		return setDescription(null);
+	}
+	 
+	private String setDescription(AbstractPlayer.PlayerClass c) {
+		return this.DESCRIPTIONS[1] + this.DESCRIPTIONS[0];
+	}
+
+	public void updateDescription(AbstractPlayer.PlayerClass c) {
+		this.description = setDescription(c);
+		this.tips.clear();
+		this.tips.add(new PowerTip(this.name, this.description));
+		initializeTips();
+	}
+	
+	public void atTurnStart() {
+		AbstractDungeon.actionManager.addToBottom(
+				new DowngradeRandomCardInDeckAction(AbstractDungeon.player,DOWN_AMT));
+	}
+	
+	public void onEquip() {
+		AbstractDungeon.player.energy.energyMaster += 1;
+	}
+	
+	public void onUnequip() {
+		AbstractDungeon.player.energy.energyMaster -= 1;
+	}
+
+	public AbstractRelic makeCopy() {
+		return new UraniumAnvil();
+	}
+	
+}
