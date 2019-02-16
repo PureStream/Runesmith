5390390b86ac132d2a108c3ffe999dfdea38998f
Blizzarre <Blizzarre@users.noreply.github.com>
2019-02-17 01:37:40 +0700

    fixed image path

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/Overpower.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/Overpower.java
@@ -18,7 +18,7 @@ public class Overpower extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "images/cards/OverPower.png"; //<-------------- need some img
+    public static final String IMG_PATH = "images/cards/Overpower.png"; //<-------------- need some img
     private static final int COST = 1;
     private static final int ATTACK_DMG = 14;
     private static final int UPGRADE_PLUS_DMG = 4;
