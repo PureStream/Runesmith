24b9b386842c4386d4c7fff0d1fe58865162a05a
Blizzarre <Blizzarre@users.noreply.github.com>
2019-03-15 11:47:56 +0700

    v0.4.0

--- the_runesmith/src/main/java/runesmith/cards/Runesmith/FrozenChisel.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/FrozenChisel.java
@@ -25,7 +25,7 @@ public class FrozenChisel extends CustomCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG_PATH = "images/cards/FrozenChisel.png"; //<-------------- need some img
     private static final int COST = 2;
-    private static final int ATTACK_DMG = 12;
+    private static final int ATTACK_DMG = 11;
     private static final int UPGRADE_PLUS_DMG = 2;
 //    private static final int STR_DOWN_AMT = 1;
     private static final int SLOW_RECOVER = 3;
--- the_runesmith/src/main/java/runesmith/cards/Runesmith/MetallurgicalResearch.java
+++ the_runesmith/src/main/java/runesmith/cards/Runesmith/MetallurgicalResearch.java
@@ -17,8 +17,9 @@ public class MetallurgicalResearch extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 1;
-    private static final int WAIT_AMT = 3;
+    private static final int COST = 2;
+//    private static final int COST_UPG = 1;
+    private static final int WAIT_AMT = 2;
     private static final int UPG_WAIT_AMT = -1;
 
     public MetallurgicalResearch() {
@@ -53,6 +54,7 @@ public class MetallurgicalResearch extends CustomCard {
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
+//            upgradeBaseCost(COST_UPG);
             upgradeMagicNumber(UPG_WAIT_AMT);
         }
     }
